967bf045946bf43de9328d439390fc49983fc02b
Mohit Sharma <mohit.sharma@team.telstra.com>
2018-06-20 21:30:14 +1000

    Initial Commit: UI implementation added

---
+++ app/src/main/java/com/mohitum/androidmvpkotlindemo/activity/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.mohitum.androidmvpkotlindemo.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.mohitum.androidmvpkotlindemo.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
